fc3630985f26d96352f49134fff741c674c22d28
Yashmandaokar <yashmandaokar7@gmail.com>
2023-12-06 10:07:24 +0100

    Working 3D mesh Parser

---
+++ tests/user_examples/FVM_3d_Unstruct/FVM_3d_Unstruct.h
@@ -0,0 +1,125 @@
+/**
+ * @file 	Yash_3d_FVM_double_mach_reflection_Unstruct.h
+ * @brief 	This is a test to show the double mach reflection in FVM.
+ * @details See https://doi.org/10.1016/j.jcp.2010.08.019 for the detailed problem setup.
+ * @author 	Zhentong Wang and Xiangyu Hu
+ */
+
+#ifndef FVM_3D_Unstruct_H
+#define FVM_3D_Unstruct_H
+#include "unstructured_mesh.h"              // shared eulerian classes for weakly-compressible and compressible fluid in FVM.
+using namespace SPH;
+using namespace std;
+//----------------------------------------------------------------------
+//	Basic geometry parameters and numerical setup.
+//----------------------------------------------------------------------
+Real DL = 4.0;                            /**< Computation domain length. */
+Real DH = 1.0;                           /**< Computation domain height. */
+Real DW = 0.1;                          /**< Computation domain width. */
+Real particle_spacing_ref = 1.0 / 240.0; /**< Initial reference particle spacing. */
+/** Domain bounds of the system. */
+BoundingBox system_domain_bounds(Vec3d(0.0, 0.0, 0.0), Vec3d(DL, DH, DW));
+//----------------------------------------------------------------------
+//	Material properties of the fluid.
+//----------------------------------------------------------------------
+Real rho0_one = 1.4;                         /**< initial density of one fluid. */
+Real u_one = 0.0;                            /**< initial velocity of one fluid in X axis. */
+Real v_one = 0.0;                            /**< initial velocity of one fluid in Y axis. */
+Real w_one = 0.0;							/**< initial velocity of one fluid in Z axis. */
+Real p_one = 1.0;                            /**< initial pressure of one fluid. */
+Real rho0_another = 8.0;                     /**< initial density of another. */
+Real u_another = 8.25 * sin(3.14159 / 3.0);  /**< initial velocity of another in X axis. */
+Real v_another = -8.25 * cos(3.14159 / 3.0); /**< initial velocity of another in Y axis. */
+Real w_another = 1; /**< initial velocity of another in Z axis. */
+Real p_another = 140.2 / 1.2;                /**< initial pressure of another. */
+Real heat_capacity_ratio = 1.4;              /**< heat capacity ratio. */
+
+//----------------------------------------------------------------------
+//	Set the file path to the data file.
+//----------------------------------------------------------------------
+std::string double_mach_reflection_Unstruct_mesh_fullpath = "./input/double_mach_reflection_3D_UnStruc_ref.msh";
+//
+//	Define geometries and body shapes
+//----------------------------------------------------------------------
+/*
+std::vector<Vecd> CreatComputationDomain()
+{
+    // geometry
+    std::vector<Vecd> computation_domain;
+    computation_domain.push_back(Vecd(0.0, 0.0, 0.0));
+    computation_domain.push_back(Vecd(0.0, DH, 0.0));
+    computation_domain.push_back(Vecd(DL, DH, 0.0));
+    computation_domain.push_back(Vecd(DL, 0.0, 0.0));
+    computation_domain.push_back(Vecd(0.0, 0.0, 0.0));
+    computation_domain.push_back(Vecd(0.0, 0.0, DW));
+    computation_domain.push_back(Vecd(0.0, DH, DW));
+    computation_domain.push_back(Vecd(DL, DH, DW));
+    computation_domain.push_back(Vecd(DL, 0.0, DW));
+    computation_domain.push_back(Vecd(0.0, 0.0, DW));
+
+    return computation_domain;
+}*/
+class WaveBody : public ComplexShape
+{
+public:
+    explicit WaveBody(const std::string& shape_name) : ComplexShape(shape_name)
+    {
+        Vecd halfsize_wave(0.5 * DH, 0.5 * DL, 0.5 * DW);
+        Transform translation_wave(halfsize_wave);
+        add<TransformShape<GeometricShapeBox>>(Transform(translation_wave), halfsize_wave);
+    }
+};
+//----------------------------------------------------------------------
+//	Case-dependent initial condition.
+//----------------------------------------------------------------------
+class DMFInitialCondition
+    : public fluid_dynamics::FluidInitialCondition
+{
+public:
+    explicit DMFInitialCondition(SPHBody& sph_body)
+        : FluidInitialCondition(sph_body), pos_(particles_->pos_), vel_(particles_->vel_),
+        rho_(particles_->rho_), p_(*particles_->getVariableByName<Real>("Pressure"))
+    {
+        particles_->registerVariable(mom_, "Momentum");
+        particles_->registerVariable(dmom_dt_, "MomentumChangeRate");
+        particles_->registerVariable(dmom_dt_prior_, "OtherMomentumChangeRate");
+        particles_->registerVariable(E_, "TotalEnergy");
+        particles_->registerVariable(dE_dt_, "TotalEnergyChangeRate");
+        particles_->registerVariable(dE_dt_prior_, "OtherEnergyChangeRate");
+        gamma_ = heat_capacity_ratio;
+    };
+    void update(size_t index_i, Real dt)
+    {
+        if (pos_[index_i][1] > tan(3.14159 / 3.0) * (pos_[index_i][0] - 1.0 / 6.0))
+        {
+            /** initial left wave pressure,momentum and energy profile */
+            rho_[index_i] = rho0_another;
+            p_[index_i] = p_another;
+            Real rho_e = p_[index_i] / (gamma_ - 1.0);
+            vel_[index_i][0] = u_another;
+            vel_[index_i][1] = v_another;
+            vel_[index_i][2] = w_another;
+            mom_[index_i] = rho_[index_i] * vel_[index_i];
+            E_[index_i] = rho_e + 0.5 * rho_[index_i] * vel_[index_i].squaredNorm();
+        }
+        else
+        {
+            rho_[index_i] = rho0_one;
+            p_[index_i] = p_one;
+            Real rho_e = p_[index_i] / (gamma_ - 1.0);
+            vel_[index_i][0] = u_one;
+            vel_[index_i][1] = v_one;
+            vel_[index_i][2] = w_another;
+            mom_[index_i] = rho_[index_i] * vel_[index_i];
+            E_[index_i] = rho_e + 0.5 * rho_[index_i] * vel_[index_i].squaredNorm();
+        }
+    }
+
+protected:
+    StdLargeVec<Vecd>& pos_, & vel_;
+    StdLargeVec<Real>& rho_, & p_;
+    StdLargeVec<Vecd> mom_, dmom_dt_, dmom_dt_prior_;
+    StdLargeVec<Real> E_, dE_dt_, dE_dt_prior_;
+    Real gamma_;
+};
+#endif // FVM_3D_Unstruct_H
---
+++ tests/user_examples/FVM_3d_Unstruct_old/FVM_3d_Unstruct_old.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file 	Yash_3d_FVM_double_mach_reflection_Unstruct.cpp
+ * @brief 	This is the compressible test for the realization of FVM in the SPHinXsys.
+ * @details We consider a double mach reflection case.
+ * @author 	Zhentong Wang and Xiangyu Hu
+ */
+#include "FVM_3d_Unstruct_old.h"
+#include "sphinxsys.h"
+using namespace SPH;
+//----------------------------------------------------------------------
+//	Main program starts here.
+//----------------------------------------------------------------------
+int main(int ac, char* av[])
+{
+    // read data from ANSYS mesh.file
+    readFluentMeshFile_3d read_mesh_data(double_mach_reflection_Unstruct_mesh_fullpath);
+    //----------------------------------------------------------------------
+   //	Build up the environment of a SPHSystem.
+   //----------------------------------------------------------------------
+    SPHSystem sph_system(system_domain_bounds, particle_spacing_ref);
+    // Handle command line arguments and override the tags for particle relaxation and reload.
+    sph_system.handleCommandlineOptions(ac, av);
+    IOEnvironment io_environment(sph_system);
+    //----------------------------------------------------------------------
+    //	Creating body, materials and particles.
+    //----------------------------------------------------------------------
+    FluidBody wave_block(sph_system, makeShared<WaveBody>("WaveBody"));
+    wave_block.defineParticlesAndMaterial<BaseParticles, CompressibleFluid>(rho0_another, heat_capacity_ratio);
+    wave_block.generateParticles<FluentParticleGeneratorInFVM>(read_mesh_data.elements_center_coordinates_, read_mesh_data.elements_volumes_);
+    wave_block.addBodyStateForRecording<Real>("Density");
+    wave_block.addBodyStateForRecording<Real>("Pressure");
+    /** Initial condition and register variables*/
+    SimpleDynamics<DMFInitialCondition> initial_condition(wave_block);
+    GhostCreationFromFluentMesh ghost_creation(wave_block, read_mesh_data.cell_lists_, read_mesh_data.point_coordinates_3D_);
+
+    // Visualization in FVM with date in cell.
+    BodyStatesRecordingInFluentMeshToVtu write_real_body_states(
+        io_environment, sph_system.real_bodies_, read_mesh_data.elements_nodes_connection_, read_mesh_data.point_coordinates_3D_);
+    write_real_body_states.writeToFile(0);
+    return 0;
+}
